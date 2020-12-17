0dc77f353fce4d9226e5e80c59b573b1bfc9d1ba
hwangseonu <hwangseonu12@naver.com>
2020-12-17 23:16:32 +0900

    init project

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class UserModel(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    student_id = models.CharField(max_length=5)
